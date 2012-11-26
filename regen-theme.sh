1cabbe0cf792649399eecf90373c9b96ec87db26
Dave Rolsky <autarch@urth.org>
2012-11-25 18:13:12 -0600

    Use scripts from node_modules dir

--- regen-theme.sh
+++ regen-theme.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-lessc ./less/my-theme.less ./compiled-style.css
+./node_modules/less/bin/lessc ./less/my-theme.less ./compiled-style.css
 cp ./less/bootstrap/img/* img/
 
 cat less/bootstrap/js/bootstrap-transition.js less/bootstrap/js/bootstrap-alert.js less/bootstrap/js/bootstrap-button.js less/bootstrap/js/bootstrap-carousel.js less/bootstrap/js/bootstrap-collapse.js less/bootstrap/js/bootstrap-dropdown.js less/bootstrap/js/bootstrap-modal.js less/bootstrap/js/bootstrap-tooltip.js less/bootstrap/js/bootstrap-popover.js less/bootstrap/js/bootstrap-scrollspy.js less/bootstrap/js/bootstrap-tab.js less/bootstrap/js/bootstrap-typeahead.js less/bootstrap/js/bootstrap-affix.js > js/bootstrap.js
 
-uglifyjs -nc -v js/bootstrap.js > js/bootstrap.min.tmp.js
+./node_modules/uglify-js/bin/uglifyjs -nc -v js/bootstrap.js > js/bootstrap.min.tmp.js
 
 rm js/bootstrap.min.tmp.js
