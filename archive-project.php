2da4800152945546314578627eca9bdbc5876247
Gregory Oschwald <goschwald@maxmind.com>
2013-06-14 20:53:14 -0700

    Removed uneeded link

--- archive-project.php
+++ archive-project.php
@@ -20,29 +20,28 @@ jQuery(document).ready(function ($) {
   $('#projects_carousel').carousel()
 });
 </script>
-<section id="primary" class="span12">
+<section id="primary" class="col-span-12">
 
-	<?php tha_content_before(); ?>
+  <?php tha_content_before(); ?>
 
-	<div id="content" role="main">
-		<?php tha_content_top();
+  <div id="content" role="main">
+    <?php tha_content_top();
 
-		if ( have_posts() ) : ?>
+if (have_posts()) : ?>
 
-   <header class="page-header">
-    <h1 class="page-title">Civic Engagement Projects</h1>
-  </header><!-- .page-header -->
-  <div style="width: 700px">
-    <div id="projects_carousel" class="carousel slide">
-     <div class="carousel-inner">
-       <?php
-       $index = 0;
-       while ( have_posts() ) {
+    <header class="page-header">
+      <h1 class="page-title">Civic Engagement Projects</h1>
+    </header><!-- .page-header -->
+    <div style="width: 700px">
+      <div id="projects_carousel" class="carousel slide">
+       <div class="carousel-inner">
+         <?php
+         $index = 0;
+    while (have_posts()) {
         the_post();
-        if (has_post_thumbnail()){
-          ?>
-          <div class="item <? if ($index === 0) echo "active" ?>">
-            <a href="<?php echo get_permalink(); ?>">
+        if (has_post_thumbnail()) {
+            ?>
+            <div class="item <?php if ($index === 0) echo "active" ?>">
               <?php the_post_thumbnail(array(700, 460)); ?>
               <div class="carousel-caption">
                 <h4><?php the_title(); ?></h4>
@@ -50,36 +49,34 @@ jQuery(document).ready(function ($) {
                   <?php the_excerpt(); ?>
                 </p>
               </div>
-            </a>
-          </div>
-          <?php
-          $index++;
+            </div>
+            <?php
+            $index++;
         }
-      }
-      ?>
+    }
+        ?>
+      </div>
+      <a class="left carousel-control" href="#projects_carousel" data-slide="prev">&lsaquo;</a>
+      <a class="right carousel-control" href="#projects_carousel" data-slide="next">&rsaquo;</a>
+
     </div>
-    <a class="left carousel-control" href="#projects_carousel" data-slide="prev">&lsaquo;</a>
-    <a class="right carousel-control" href="#projects_carousel" data-slide="next">&rsaquo;</a>
 
-  </div>
+    <?php wp_tag_cloud(array('taxonomy' => 'topic')) ?>
 
-  <?php wp_tag_cloud( array( 'taxonomy' => 'topic' ) ) ?>
+    <?php
+    the_bootstrap_content_nav();
+else :
+     get_template_part('/partials/content', 'not-found');
+endif;
 
-  <?php
-  the_bootstrap_content_nav();
-  else :
-   get_template_part( '/partials/content', 'not-found' );
- endif;
-
- tha_content_bottom(); ?>
-</div>
+   tha_content_bottom(); ?>
+ </div>
 </div><!-- #content -->
 <?php tha_content_after(); ?>
 </section><!-- #primary -->
 
 <?php
 get_footer();
-
 
 /* End of file archive.php */
 /* Location: ./wp-content/themes/the-bootstrap/archive.php */
