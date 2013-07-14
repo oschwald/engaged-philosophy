0e4c4b254898af737219012e1c93736e19678257
Gregory Oschwald <goschwald@maxmind.com>
2013-07-14 17:53:59 -0600

    Fixed search

--- search.php
+++ search.php
@@ -24,7 +24,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) {
 				the_post();
-				get_template_part( '/partials/content', get_post_format() );
+				get_template_part( '/partials/content', 'summary' );
 			}
 			the_bootstrap_content_nav();
 		else :
