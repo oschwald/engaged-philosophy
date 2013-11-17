bdf0879276e682ff8dd4f2240df1a92e6b263052
Gregory Oschwald <goschwald@maxmind.com>
2013-11-17 12:00:34 -0800

    Ugly survey code

--- templates/home.php
+++ templates/home.php
@@ -58,6 +58,62 @@ jQuery(document).ready(function ($) {
           the_content();
           ?>
         </div>
+        <div class="survey-box">
+        <!--BEGIN QUALTRICS FEEDBACK LINK-->
+<script type="text/javascript">
+var Qualtrics_SV_01EXpZ4YggB9oYR = {
+  url:document.URL,
+  survey:"http://stkate.az1.qualtrics.com/SE/?SID=SV_01EXpZ4YggB9oYR",
+  popup:function(width,height) {
+    link=Qualtrics_SV_01EXpZ4YggB9oYR.survey+"&url="+Qualtrics_SV_01EXpZ4YggB9oYR.url;
+    options = "menubar=no,width="+width+",height="+height+",toolbar=no,location=no,status=no,scrollbars=yes"
+    var popupwindow = window.open(link,'Title',options);
+    popupwindow.href+='&test';
+  },
+  showTip:function() {
+    if(!document.getElementById('SV_01EXpZ4YggB9oYR'+'_tooltip')){
+      var new_link = document.getElementById('SV_01EXpZ4YggB9oYR'+'_feedbacklink');
+      var tip = document.createElement('div');
+      tip.style.fontSize='8pt';
+      tip.style.position='absolute';
+      tip.style.backgroundColor = '#FFFFC7';
+      tip.style.border='1px solid #FFDC81';
+      tip.style.color='#777777';
+      tip.style.fontFamily='Arial';
+      tip.style.whiteSpace='nowrap';
+      tip.style.textDecoration='none';
+      tip.style.padding='2px';
+      tip.id='SV_01EXpZ4YggB9oYR'+'_tooltip';
+      tip.innerHTML='Let us know what you think about the Engaged Philosophy website';
+      document.body.appendChild(tip);
+      if (new_link.addEventListener){new_link.addEventListener('mousemove', Qualtrics_SV_01EXpZ4YggB9oYR.moveTip, false);} elseif (new_link.attachEvent){ new_link.attachEvent('onmousemove', Qualtrics_SV_01EXpZ4YggB9oYR.moveTip);}
+    }
+  },
+  moveTip:function(e) {
+    if(document.getElementById('SV_01EXpZ4YggB9oYR'+'_tooltip')) {
+      var tip=document.getElementById('SV_01EXpZ4YggB9oYR'+'_tooltip');
+      var mouseX;var mouseY;
+      if (e.pageX){mouseX=e.pageX;mouseY=e.pageY;} else{mouseX=e.clientX+document.documentElement.scrollLeft;mouseY=e.clientY+document.documentElement.scrollTop;}
+      if (mouseY-25<0){tip.style.top=mouseY + 15 +'px';} else {tip.style.top=(mouseY-25)+'px';}
+      if (mouseX+15+tip.offsetWidth>window.innerWidth){tip.style.left=(window.innerWidth-tip.offsetWidth)+'px';} else{tip.style.left=(mouseX+15)+'px';}
+    }
+  },
+  removeTip:function() {
+    var tip = document.getElementById('SV_01EXpZ4YggB9oYR'+'_tooltip');tip.parentNode.removeChild(tip);
+    var new_link = document.getElementById('SV_01EXpZ4YggB9oYR'+'_feedbacklink');
+    if (new_link.addEventListener){new_link.removeEventListener('mousemove', Qualtrics_SV_01EXpZ4YggB9oYR.moveTip, false);} elseif (new_link.detachEvent){ new_link.detachEvent('onmousemove', Qualtrics_SV_01EXpZ4YggB9oYR.moveTip);}
+  }
+}
+var link=Qualtrics_SV_01EXpZ4YggB9oYR.survey+"&url="+Qualtrics_SV_01EXpZ4YggB9oYR.url;
+document.write("<a id='SV_01EXpZ4YggB9oYR"+"_feedbacklink' href='#' onclick='Qualtrics_SV_01EXpZ4YggB9oYR.popup(900,600); return false;'>How are we doing?</a>");
+var new_link = document.getElementById('SV_01EXpZ4YggB9oYR'+'_feedbacklink');
+new_link.style.textDecoration='underline';
+new_link.onmouseover=function(){Qualtrics_SV_01EXpZ4YggB9oYR.showTip();};
+new_link.onmouseout=function(){Qualtrics_SV_01EXpZ4YggB9oYR.removeTip();};</script>
+<noscript><a target="_blank" href="http://stkate.az1.qualtrics.com/SE/?SID=SV_01EXpZ4YggB9oYR">Feedback</a><br/>
+</noscript>
+<!--END QUALTRICS FEEDBACK LINK-->
+        </div>
       </div>
     </div>
     <div class="row">
