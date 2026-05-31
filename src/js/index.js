0256411560ddaf8c02a2d25e316815414ed309ce
Gregory Oschwald <oschwald@gmail.com>
2026-05-30 17:02:19 -0700

    refactor: trim dead theme scripts

--- src/js/index.js
+++ src/js/index.js
@@ -6,13 +6,6 @@ import * as bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
 window.bootstrap = bootstrap;
 
 // Import custom JavaScript modules
-import "./theme-scripts.js";
 import "./carousel.js";
-import "./custom-header.js";
+import "./facebook-sdk.js";
 import "./emdash-save-gate.js";
-
-// Initialize theme functionality
-document.addEventListener("DOMContentLoaded", function () {
-	// Initialize any theme-specific JavaScript here
-	// Theme loaded successfully
-});
