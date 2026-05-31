9cdcffd370df6f0adcb6ecb73042c8c02e9b4cf2
Gregory Oschwald <oschwald@gmail.com>
2026-05-30 17:14:46 -0700

    fix: use EmDash favicon setting

---
+++ src/pages/site.webmanifest.ts
@@ -0,0 +1,44 @@
+import { getRuntimeSiteSettings } from "../lib/content";
+
+export async function GET() {
+	const settings = await getRuntimeSiteSettings();
+	const siteTitle = settings?.title || "Engaged Philosophy";
+	const favicon = settings?.favicon;
+	const faviconIcon = favicon?.url
+		? {
+				src: favicon.url,
+				sizes:
+					favicon.width && favicon.height
+						? `${favicon.width}x${favicon.height}`
+						: "any",
+				...(favicon.contentType ? { type: favicon.contentType } : {}),
+			}
+		: {
+				src: "/favicon.svg",
+				sizes: "any",
+				type: "image/svg+xml",
+			};
+
+	return new Response(
+		JSON.stringify(
+			{
+				name: siteTitle,
+				short_name:
+					siteTitle === "Engaged Philosophy" ? "EngagedPhil" : siteTitle,
+				description:
+					settings?.tagline || "Civic Engagement in Philosophy Classes",
+				icons: [faviconIcon],
+				theme_color: "#fd7e14",
+				background_color: "#ffffff",
+				display: "standalone",
+			},
+			null,
+			"\t",
+		),
+		{
+			headers: {
+				"Content-Type": "application/manifest+json; charset=utf-8",
+			},
+		},
+	);
+}
