1f5a50ac0aafe626e83323927bd4154850408ac6
Gregory Oschwald <oschwald@gmail.com>
2026-07-24 16:27:25 -0700

    refactor: use Bootstrap carousel data API

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
 // Main JavaScript entry point
-// Import Bootstrap JavaScript and expose globally
-import * as bootstrap from "bootstrap/dist/js/bootstrap.bundle.min.js";
+// Load only the Bootstrap behaviors used by the public theme.
+import "bootstrap/js/dist/carousel";
+import "bootstrap/js/dist/collapse";
+import "bootstrap/js/dist/dropdown";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-// Make Bootstrap available globally for WordPress customizer and other scripts
-window.bootstrap = bootstrap;
-
 // Import custom JavaScript modules
-import "./carousel.js";
 import "./emdash-save-gate.js";
