77d5462cbb8ee3727fbeb1515b9334cb2bb85518
Gregory Oschwald <oschwald@gmail.com>
2026-07-25 15:15:11 -0700

    fix: publish canonical HTTPS URLs

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -1,6 +1,7 @@
 export const SITE_TITLE_FALLBACK = "Engaged Philosophy";
 export const SITE_TAGLINE_FALLBACK = "Civic Engagement in Philosophy Classes";
-export const WORDPRESS_SITE_URL = "https://www.engagedphilosophy.com";
+export const PUBLIC_SITE_URL = "https://www.engagedphilosophy.com";
+export const WORDPRESS_SITE_URL = PUBLIC_SITE_URL;
 export const PUBLIC_MEDIA_URL = "https://media.engagedphilosophy.com";
 export const CLOUDFLARE_ACCESS_TEAM_DOMAIN =
 	"engaged-philosophy.cloudflareaccess.com";
