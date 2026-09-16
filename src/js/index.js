b71a8d94b9f5ddf38f2aaa66506948446dbcb71f
Gregory Oschwald <oschwald@gmail.com>
2026-09-15 20:00:01 -0700

    perf: replace search icon font with shared SVG

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@
 import "bootstrap/js/dist/carousel";
 import "bootstrap/js/dist/collapse";
 import "bootstrap/js/dist/dropdown";
-import "bootstrap-icons/font/bootstrap-icons.css";
 
 // Import custom JavaScript modules
 import "./emdash-save-gate.js";
